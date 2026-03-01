08cea075ca526e0f926b720e16be170ead905a4a
aaban <aabanaahmed@gmail.com>
2026-02-28 23:59:53 -0500

    RegionalCode put in separate teleop file.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/RegionalsTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/RegionalsTeleOp.java
@@ -1,42 +1,28 @@
 package org.firstinspires.ftc.teamcode.opmodes;
 
-import android.service.controls.Control;
-
 import com.acmerobotics.dashboard.config.Config;
 import com.acmerobotics.roadrunner.Pose2d;
 import com.acmerobotics.roadrunner.PoseVelocity2d;
 import com.acmerobotics.roadrunner.Vector2d;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
-import com.qualcomm.robotcore.hardware.CRServo;
-import com.qualcomm.robotcore.hardware.CRServoImplEx;
 import com.qualcomm.robotcore.hardware.DcMotorImplEx;
-import com.qualcomm.robotcore.hardware.DcMotorSimple;
-import com.qualcomm.robotcore.hardware.ServoImplEx;
+import com.qualcomm.robotcore.hardware.Servo;
 
 import org.firstinspires.ftc.teamcode.MecanumDrive;
 
 import dev.nextftc.control.ControlSystem;
-import dev.nextftc.control.KineticState;
 import dev.nextftc.control.feedback.PIDCoefficients;
-import dev.nextftc.control.feedback.PIDController;
-import dev.nextftc.hardware.impl.MotorEx;
-
-import dev.nextftc.control.ControlSystem;
-import dev.nextftc.core.commands.Command;
-import dev.nextftc.core.subsystems.Subsystem;
-import dev.nextftc.hardware.controllable.RunToVelocity;
 import dev.nextftc.hardware.impl.MotorEx;
 
 
 @TeleOp
 @Config
-public class MainTeleOp extends OpMode {
+public class RegionalsTeleOp extends OpMode {
     MecanumDrive drive;
-    DcMotorImplEx intakeMotor;
     DcMotorImplEx outtakeMotor;
     MotorEx outtakeMotorEx;
-    CRServoImplEx servo1;
+    Servo servo1;
     ControlSystem controller;
     boolean farOrClose;
     double motorPower;
@@ -51,12 +37,10 @@ public class MainTeleOp extends OpMode {
     @Override
     public void init() {
         drive = new MecanumDrive(hardwareMap, new Pose2d(0, 0, 0));
-        intakeMotor = hardwareMap.get(DcMotorImplEx.class, "intakeMotor");
         outtakeMotor = hardwareMap.get(DcMotorImplEx.class, "outtakeMotor");
         outtakeMotorEx = new MotorEx(outtakeMotor);
-        intakeMotor.setDirection(DcMotorSimple.Direction.REVERSE);
         farOrClose = true; // true is far, false is close
-        servo1 = hardwareMap.get(CRServoImplEx.class, "servo1");
+        servo1 = hardwareMap.get(Servo.class, "servo1");
         servoOn = false;
         adjustable = false;
         output = 0;
@@ -101,15 +85,15 @@ public class MainTeleOp extends OpMode {
 //                }
 //            }
 //        } else {
-        if (gamepad1.right_trigger > 0.1) {
-            if (farOrClose) {
-                outtakeMotor.setPower(.8);
+            if (gamepad1.right_trigger > 0.1) {
+                if (farOrClose) {
+                    outtakeMotor.setPower(.8);
+                } else {
+                    outtakeMotor.setPower(.67);
+                }
             } else {
-                outtakeMotor.setPower(.67);
+                outtakeMotor.setPower(0);
             }
-        } else {
-            outtakeMotor.setPower(0);
-        }
 //        }
 //
 //        if (gamepad1.options) {
@@ -123,25 +107,11 @@ public class MainTeleOp extends OpMode {
         }
 
         if (gamepad1.a) {
-            servo1.setPower(-.5);
+            servo1.setPosition(1);
         }
 
-
-        if (gamepad1.xWasPressed()) {
-            servo1.setPower(.75);
-        }
-        else if (gamepad1.xWasReleased())
-        {
-            servo1.setPower(0);
-        }
-
-
-        if (gamepad1.left_trigger > 0.1) {
-            intakeMotor.setPower(.85);
-        } else if (gamepad1.b){
-            intakeMotor.setPower(-.67);
-        } else {
-            intakeMotor.setPower(0);
+        if (gamepad1.a) {
+            servo1.setPosition(0);
         }
 
         double y = -gamepad1.left_stick_y ; // negate to make outtake front
